04c89bb4134388bbc875c20aac8360af7fce8167
Animesh Maiti <a.maiti770@gmail.com>
2022-12-31 16:40:22 +0530

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const navMenu = document.getElementById('nav-menu'),
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
-    navToggle.addEventListener('click', () =>{
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
@@ -13,17 +13,17 @@ if (navToggle) {
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if (navClose) {
-    navClose.addEventListener('click', () =>{
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
-    })    
+    })
 }
 
 /*==================== REMOVE MENU MOBILE ====================*/
 const navLink = document.querySelectorAll('.nav_link')
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     //when we click on each nav_link, we remove the show-menu class
-    navMenu.classList.remove('show-menu') 
+    navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
@@ -33,35 +33,115 @@ const skillsContent = document.getElementsByClassName('skills_content'),
 function toggleSkills() {
     let itemClass = this.parentNode.className
 
-    for (i =0; i <skillsContent.length; i++) {
-        skillsContent[i].className = 'skills_content skills_close'        
+    for (i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills_content skills_close'
     }
     if (itemClass === 'skills_content skills_close') {
         this.parentNode.className = 'skills_content skills_open'
     }
 }
-skillsHeader.forEach((el)=>{
+skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
 /*==================== QUALIFICATION TABS ====================*/
-
+const tabs = document.querySelectorAll('[data-target]'),
+    tabContents = document.querySelectorAll('[data-content]')
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
+const modalViews = document.querySelectorAll('.services_modal'),
+    modalBtns = document.querySelectorAll('.services_button'),
+    modalClose = document.querySelectorAll('.services_modal-close')
 
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+modalClose.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
+        })
+    })
+})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+let swiperPortfolio = new Swiper(".portfolio_container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+    mousewheel: true,
+    keyboard: true,
+});
 
 /*==================== TESTIMONIAL ====================*/
+let swiperTestimonial = new Swiper(".testimonial_container", {
+    loop: true,
+    // grabCursor:true,
+    spaceBetween: 48,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable:true,
+        dynamicBullets: true,
+    },
+    breakPoints:{
+        568:{
+            slidesPreview: 2,
+        }
+    },
+    // mousewheel: true,
+    keyboard: true,
+});
 
+/*===================== Email js =====================*/
+function sendMail(){
+    var params = {
+        from_name: document.getElementById("full_name").value,
+        email_id: document.getElementById("email_id").value,
+        project: document.getElementById("project_title").value,
+        message: document.getElementById("message").value
+    }
+    // console.log(document.getElementById("full_name").value,document.getElementById("email_id").value,document.getElementById("message").value)
+    
+    emailjs.send("service_liaf60n","template_tu2ho2j",params)
+    .then(function(result) {
+        alert("thanks " +document.getElementById("full_name").value);
+        console.log('SUCCESS!');
+    }, function(error) {
+        console.log('FAILED...', error);
+    });
+}
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+/*==================== CHANGE BACKGROUND HEADER ====================*/
 
 
-/*==================== SHOW SCROLL UP ====================*/ 
+/*==================== SHOW SCROLL UP ====================*/
 
 
 /*==================== DARK LIGHT THEME ====================*/ 
